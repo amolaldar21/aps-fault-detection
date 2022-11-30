990da6739491c9a958b43105a72d3d7bd6135b18
amolaldar21 <amol21.seo@gmail.com>
2022-11-30 10:52:11 +0530

    update

---
+++ sensor/logger.py
@@ -0,0 +1,21 @@
+import logging
+import os
+from datetime import datetime
+import os
+# log file name
+log_file_name = f"{datetime.now().strftime('%m%d%Y_%H%M%S')}.log"
+# log directory
+log_file_dir=os.path.join(os.getcwd(),"logs")
+# create folder if not exist
+os.makedir(:log_file_dir,exist_ok=True)
+
+# log file name
+
+log_file_path = os.path.join(log_file_dir,log_file_name)
+
+logging.basicConfig(
+filename=log_file_path,
+format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+level=logging.INFO,
+
+)
--- setup.py
+++ setup.py
@@ -1,6 +1,15 @@
 from setuptools import find_packages,setup
+from typing import List
 
-def get_requirements():...
+REQUIREMENT_FILE_NAME ="requirements.txt"
+HYPEN_E_DOT ="-e ."
+def get_requirements() :
+    with open (REQUIREMENT_FILE_NAME)as requirement_file:
+        requirement_list=requirement_file.readlines()
+    requirement_list =[requirement_name.replace("\n","") for requirement_name in requirement_list]
+    if HYPEN_E_DOT in requirement_list:
+        requirement_list.remove(HYPEN_E_DOT)
+    return requirement_list
 
 
 
@@ -11,7 +20,16 @@ setup(
     author='ineuron',
     author_email='amol21.seo@gmail.com',
     packages=find_packages(),
-    install_requires=get_requirements(),
+    install_requires=get_requirements()
 
 
     )
+
+'''def get_requirements():
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
+        requirement_list=requirement_file.readlines()
+        requirement_list= [requiremnet_name.replace("\n","") for requiement_name in requirement_list]
+        if HYPEN-E-DOT in requiement_list:
+            requirement_list.remove(HYPEN_E-DOT)
+        return requirement_list
+        '''
